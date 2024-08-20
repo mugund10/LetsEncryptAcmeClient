6783cf397830dff3a93cb163d52066fef08afa03
mugund10 <bjmugundhan@gmail.com>
2024-08-21 02:34:09 +0530

    added write and read pem functionality

--- cmd/acmeclient/main.go
+++ cmd/acmeclient/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/mugund10/LetsEncryptAcmeClient"
@@ -11,6 +12,10 @@ func main() {
 	chechError(err)	
 	saves := leacme.SaveKey(keys,"test.pem")
 	chechError(saves)
+
+	Pkey , err := leacme.ReadKey("test.pem")
+	chechError(err)
+	fmt.Println(Pkey)
 }
 
 func chechError(err error){
