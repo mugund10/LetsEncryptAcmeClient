665f3e75bd560ff02e3d932439abb96135b5955f
mugund10 <bjmugundhan@gmail.com>
2024-08-23 02:08:55 +0530

    not finished fullt

--- leacme/leacme.go
+++ leacme/leacme.go
@@ -5,43 +5,33 @@ import (
 	"crypto/rsa"
 	"fmt"
 
-	"github.com/mugund10/LetsEncryptAcmeClient/account"
+	"github.com/mugund10/LetsEncryptAcmeClient/Account"
 	"github.com/mugund10/LetsEncryptAcmeClient/orders"
 	"golang.org/x/crypto/acme"
 )
 
-// defaults
+// endpoints
 var (
 	productionurl string = "https://acme-v02.api.letsencrypt.org/directory"
 	stagingurl    string = "https://acme-staging-v02.api.letsencrypt.org/directory"
-
-// filename      string = "orgAccount.json"
 )
 
 // default url is staging
-var url *string = &stagingurl
-// switches the url to production url
-func Switchurl() {
-	url = &productionurl
-}
-// resets the url back to staging url
-func Reseturl() {
-	url = &stagingurl
-}
-// changes the url to given url
-func Newurl(nurl string) {
-	url = &nurl
-}
+var url *string
 
 type client struct {
 	acme_client *acme.Client
 }
 
-//creates new client with given private key
-//to change the url from production to staging call leacme.Reseturl()
-//or staging to production call leacme.Switchurl()
-//for custom url call lecame.Newurl()
-func NewClient(pkey *rsa.PrivateKey) client {
+// creates new client with given private key
+// for stagingUrl ("https://acme-staging-v02.api.letsencrypt.org/directory") value must be true
+// if its false productionUrl will be used  productionurl string "https://acme-v02.api.letsencrypt.org/directory"
+func NewClient(pkey *rsa.PrivateKey, stagingUrl bool) client {
+	if stagingUrl {
+		url = &stagingurl
+	} else {
+		url = &productionurl
+	}
 	var ca client
 	client := acme.Client{Key: pkey, DirectoryURL: *url}
 	ca.acme_client = &client
@@ -52,8 +42,8 @@ func NewClient(pkey *rsa.PrivateKey) client {
 // retrieves old registered account with the given private key
 // ( for contactaddress use this "mailto:example@example.com" format )
 func (ca *client) RegisterAccount(accountName string, contactaddress string) {
-
-	account := account.New(accountName, contactaddress)
+	//here account is variable name and Account is package name
+	account := Account.New(accountName, contactaddress)
 	if err := account.Load(); err != nil {
 		fmt.Printf("no account found, (error : %s)\n", err)
 		fmt.Printf("So Registering new account \n")
@@ -63,15 +53,11 @@ func (ca *client) RegisterAccount(accountName string, contactaddress string) {
 				fmt.Println(err)
 			}
 		}
-		
-	} 
-	//return account
+	}
 }
 
-func (ca *client) OrderDomain(domainAddress string){
+func (ca *client) Order4Domain(domainAddress string) {
 	order := orders.New(domainAddress)
-	order.Make(context.Background(),ca.acme_client)
+	order.Create(context.Background(), ca.acme_client)
+	order.Finish(ca.acme_client)
 }
-
-
-
