01cdcd64cfd8c993e6b0fad9f0b698f1891aed19
mugund10 <bjmugundhan@gmail.com>
2024-08-22 23:19:53 +0530

    csr implemented

--- keys/keys.go
+++ keys/keys.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
+	"crypto/x509/pkix"
 	"encoding/pem"
 	"errors"
 	"fmt"
@@ -23,7 +24,7 @@ func New() keyMan {
 }
 
 // generates random rsa keys with 2048 bits
-func(km *keyMan) KeyGen(keyname string) error {
+func (km *keyMan) RsaGen(keyname string) error {
 	key, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		return err
@@ -36,7 +37,7 @@ func(km *keyMan) KeyGen(keyname string) error {
 // use keys.LoadKey() first to ensure there is no key present locally in the same name
 // otherwise it will overwrite the old key with same name
 // saves the RSA PrivateKey into pem format,
-func (km *keyMan) SaveKey() error {
+func (km *keyMan) SaveAsPem() error {
 	keyname := fmt.Sprintf("%s.pem", km.name)
 	pemKey := &pem.Block{
 		Type:  "RSA PRIVATE KEY",
@@ -53,7 +54,7 @@ func (km *keyMan) SaveKey() error {
 }
 
 // loads the pemfile into RSA PrivateKey format
-func (km *keyMan) LoadKey() error {
+func (km *keyMan) LoadPem() error {
 	keyname := fmt.Sprintf("%s.pem", km.name)
 	//open the pemfile
 	file, err := os.Open(keyname)
@@ -79,4 +80,23 @@ func (km *keyMan) LoadKey() error {
 	}
 	km.Private = pkey
 	return nil
+}
+
+//default implementation of csr 
+//due to lack of study
+func (km *keyMan) CsrGen(domains []string) ([]byte, error) {
+	req := &x509.CertificateRequest{
+		Subject: pkix.Name{
+			CommonName: domains[0],
+			Country:    []string{"IN"},
+		},
+		DNSNames:           domains,
+		SignatureAlgorithm: x509.SHA256WithRSA,
+	}
+	// Generate the CSR
+	csrBytes, err := x509.CreateCertificateRequest(rand.Reader, req, km.Private)
+	if err != nil {
+		return nil, err
+	}
+	return csrBytes , nil
 }
